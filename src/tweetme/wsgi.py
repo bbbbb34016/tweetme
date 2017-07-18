1fdcf989b4095e6a9fd4a21921ebbae806080166
licheng <bbbbb34016@gmail.com>
2017-07-18 23:47:49 +0800

    change something in requirements.txt

--- src/tweetme/wsgi.py
+++ src/tweetme/wsgi.py
@@ -15,7 +15,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweetme.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-
+#from dj_static import Cling
 application = get_wsgi_application()
 application = WhiteNoise(application)
 
