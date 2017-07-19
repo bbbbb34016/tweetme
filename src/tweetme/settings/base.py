3ac70afdeca45c9b0f96f089817dd33625e65a45
licheng <bbbbb34016@gmail.com>
2017-07-19 23:49:58 +0800

    test

--- src/tweetme/settings/base.py
+++ src/tweetme/settings/base.py
@@ -124,7 +124,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-db_from_env = dj_database_url(default='DATABASE_URL_HERE')
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
