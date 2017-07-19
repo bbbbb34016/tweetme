fe5ec969667de987083403b86545fa0d16cac6cc
licheng <bbbbb34016@gmail.com>
2017-07-19 22:07:58 +0800

    add procfile.windows

--- src/tweetme/settings/base.py
+++ src/tweetme/settings/base.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'tweetme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'debug':True,
