a8c14e3b0fa6b656b5d95cc176d03ea6620d9acf
licheng <bbbbb34016@gmail.com>
2017-07-19 21:09:04 +0800

    edit base.py

--- src/tweetme/settings/base.py
+++ src/tweetme/settings/base.py
@@ -130,5 +130,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static-storage")
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,"static-serve")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static-serve")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
